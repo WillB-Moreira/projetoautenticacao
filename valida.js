56e518cfc3826646ee15ef0fd352db6fda72400b
WillB-Moreira <wmup73@gmail.com>
2022-07-27 11:46:30 -0300

    adicionando arquivos

---
+++ valida.js
@@ -0,0 +1,29 @@
+
+    /* A função é um trecho de código (algoritmo) que realiza uma tarefa específica*/
+
+    function validar(){
+        //declara as variáveis
+        //O tipo é implícito 
+        //JS é fracamente tipada
+        var login;
+        var senha;
+        //Pegar o elemento
+        login = document.getElementById("login").value;
+        senha = document.getElementById("senha").value;
+        if(login === ''){
+            alert("login obrigatório");
+            return false;
+        }else if(senha === ''){
+            alert ("senha obrigatória");
+            return false;
+        }else if(login != "SENAI"){
+                alert("Login Inválido");
+                return false;
+            }else if(senha != 123){
+                alert("Senha Inválida");
+                return false;
+            }else{
+                window.sessionStorage.setItem('login',login);
+                return true;
+            }
+        }
